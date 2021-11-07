2358700fc1e0a79cce0b3e21c02dd860f3e7ca3d
shamilkeheliya <shamilkeheliya@users.noreply.github.com>
2021-11-07 22:44:01 +0530

    apps created

---
+++ Donator App/kindful_donator/android/app/src/main/kotlin/app/kindful_donator/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.kindful_donator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Organization App/kindful_organization/android/app/src/main/kotlin/app/kindful_organization/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.kindful_organization
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
