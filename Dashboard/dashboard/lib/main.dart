ac06bdff40e05a1e748285f077134e6e343cee55
shamilkeheliya <shamilkeheliya@gmail.com>
2022-02-23 01:49:57 +0530

    login done

--- Dashboard/dashboard/lib/main.dart
+++ Dashboard/dashboard/lib/main.dart
@@ -1,20 +1,34 @@
+import 'package:dashboard/utilities/const.dart';
+import 'package:dashboard/views/login.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+const firebaseConfig = FirebaseOptions(
+  apiKey: "AIzaSyBwi8pKLqKXhIq3lzJEvL3uT57hu7mJI8Y",
+  authDomain: "kindful-6b6dd.firebaseapp.com",
+  projectId: "kindful-6b6dd",
+  storageBucket: "kindful-6b6dd.appspot.com",
+  messagingSenderId: "14056090943",
+  appId: "1:14056090943:web:0bed67d2e7aedf5e5e778a",
+  measurementId: "G-M9K621XQ4B",
+);
+
+Future<void> main() async {
+  await Firebase.initializeApp(options: firebaseConfig);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dashboard',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
+      home: Login(),
     );
   }
 }
---
+++ Dashboard/dashboard/lib/views/home.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(FirebaseAuth.instance.currentUser!.uid);
+  }
+}
